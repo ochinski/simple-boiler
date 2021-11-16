8921ce557f2ca56e49e31c17cb715c41b65e2169
Robert Ochinski <r.j.ochinski@gmail.com>
2021-11-16 11:40:45 -0500

    initial commit

---
+++ esbuild.config.js
@@ -0,0 +1,21 @@
+import esbuildServe from 'esbuild-serve';
+
+esbuildServe(
+	{
+		entryPoints: ['src/app.tsx'],
+		minify: true,
+		sourcemap: true,
+		bundle: true,
+		define: {
+			'process.env.NODE_ENV': '"production"',
+			'process.env.DEBUG': 'false',
+		},
+		outfile: 'public/bundle.js',
+	},
+    {
+        // serve options (optional)
+        port: 7000,
+        root: '.',
+        live: true,
+    }
+);
